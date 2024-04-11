6a43a7c578ea2fb67dc5b7103ace6147351a4a15
Rahul Saravanabavan <142858065+rahulsaravanabavan@users.noreply.github.com>
2024-04-10 21:15:52 -0400

    Add files via upload
    Added every changed files

---
+++ README.md
@@ -0,0 +1,14 @@
+# Web 2
+
+Public Transportation Routing in Lindsay, Ontario
+
+## Overview
+
+This website was created as a deliverable for GEOM99 Web GIS Development course at Sir Sandford Fleming College Frost Campus
+
+## Team
+
+- Athulya Sabu
+- Rahul Saravanabavan
+- Helen Plesko
+- Sharmila Wagle
---
+++ script.js
@@ -0,0 +1,24 @@
+//- Owner : Rahul Saravanabavan
+//- Date  : Nov 28 (template)
+//- From  : https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
+// End of Rahul Saravanabavan
+
+//- Owner : Thiluxsaan Puvanendran
+//- Date  : Nov 6 (template)
+//- From  : https://www.w3schools.com/howto/howto_js_toggle_dark_mode.asp
+
+function Modedark() {
+	var element = document.body;
+	element.classList.toggle("dark-mode");
+}
+// End of Thiluxsaan Puvanendran
+
+
